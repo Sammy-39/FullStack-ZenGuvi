58d753203066eacfd8d7b74b0cd034e6da5ea49b
sammy <o.samy39@gmail.com>
2020-12-09 10:53:13 +0530

    Day-5 tasks commit-2

--- D5-08-12-2020/array-methods.js
+++ D5-08-12-2020/array-methods.js
@@ -6,44 +6,34 @@ req.send()
 req.onload = () =>{
     var data = JSON.parse(req.response)
     if(req.status===200){
-       console.log("Countries in Asia region")
+       console.log("1.Countries in Asia region")
        var asiaRegion = data.filter((x)=>{return x.region==='Asia'})
        var op = []
-       for(var i in asiaRegion){
-           op.push(asiaRegion[i].name)
-        }
-        console.log(op)
+       asiaRegion.forEach((x)=>{op.push(x.name)})
+       console.log(op)
 
-        console.log("Countries with population less than 2 lacs")
+        console.log("2.Countries with population less than 2 lacs")
         var l2lpop = data.filter((x)=>{return x.population<200000})
         op = []
-        for(var i in l2lpop){
-            op.push(l2lpop[i].name)
-        }
+        l2lpop.forEach((x)=>{op.push(x.name)})
         console.log(op)
 
-        console.log('Name-Capital-Flag')
+        console.log('3.Name-Capital-Flag')
         data.forEach((x)=>{
-            console.log("Name: "+x.name)
-            console.log("Capital: "+x.capital)
-            console.log("Flag: "+x.flag)
+            console.log(`Name: ${x.name} Capital: ${x.capital} Flag: ${x.flag}`)
         })
 
-        console.log("Total population of all countries")
-        var pop = []
-        data.forEach((x)=>{pop.push(x.population)})
-        console.log(pop.reduce((p,c)=>{return p+c}))
+        console.log("4.Total population of all countries")
+        console.log(data.reduce((a,c)=>{return a+c.population},0))
 
-        console.log("Countries using US Dollars as currency")
+        console.log("5.Countries using US Dollars as currency")
         op = []
         var uscur = data.filter((x)=>{
             return x.currencies.some((y)=>{
                 return y.code==="USD"
             })
         })
-        for(var i in uscur){
-              op.push(uscur[i].name)
-        }
+        uscur.forEach((x)=>{op.push(x.name)})
         console.log(op)
     }
     else{
